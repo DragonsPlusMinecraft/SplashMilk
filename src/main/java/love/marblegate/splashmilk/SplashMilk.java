722167c73b1a720f7fdfb67d72c27c048a7ba280
白门守望者 <marblegatekeeper@outlook.com>
2021-12-26 14:45:47 +0800

    First commit

---
+++ src/main/java/love/marblegate/splashmilk/SplashMilk.java
@@ -0,0 +1,11 @@
+package love.marblegate.splashmilk;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("splash_milk")
+public class SplashMilk {
+
+   public static String MOD_ID = "splash_milk";
+}
