9e5d62546336fbfdcbf11bacc471fe3f5f01551e
MarbleGate <marblegatekeeper@outlook.com>
2024-10-22 09:57:56 +0800

    Fix recipe

--- neoforge/src/main/java/plus/dragons/splashmilk/neoforge/SplashMilk.java
+++ neoforge/src/main/java/plus/dragons/splashmilk/neoforge/SplashMilk.java
@@ -5,12 +5,15 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import plus.dragons.splashmilk.neoforge.registry.EntityRegistry;
 import plus.dragons.splashmilk.neoforge.registry.ItemRegistry;
 import plus.dragons.splashmilk.neoforge.registry.ParticleTypeRegistry;
 
+@EventBusSubscriber(modid = SplashMilk.MOD_ID)
 @Mod(SplashMilk.MOD_ID)
 public class SplashMilk {
     public final static String MOD_ID = "splash_milk";
@@ -21,10 +24,10 @@ public class SplashMilk {
         ParticleTypeRegistry.PARTICLE_TYPES.register(modEventbus);
 
         modEventbus.addListener(ItemRegistry::addToCreativeTab);
-        modEventbus.addListener(this::milking);
     }
 
-    private void milking(PlayerInteractEvent.EntityInteract event){
+    @SubscribeEvent
+    private static void milking(PlayerInteractEvent.EntityInteract event){
         if(event.getEntity().getWorld().isClient()) return;
         if(event.getTarget() instanceof CowEntity && event.getTarget().isAlive()){
             Hand hand = event.getHand();
