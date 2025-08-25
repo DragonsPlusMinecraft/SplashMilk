ae90bfec2c83a1834ed83f1f1c791d1f5438f0b3
MarbleGate <marblegatekeeper@outlook.com>
2025-08-25 09:21:29 +0800

    Update NeoForge to Recommended

--- neoforge/src/main/java/plus/dragons/splashmilk/neoforge/registry/RendererRegistry.java
+++ neoforge/src/main/java/plus/dragons/splashmilk/neoforge/registry/RendererRegistry.java
@@ -9,7 +9,7 @@ import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import plus.dragons.splashmilk.neoforge.SplashMilk;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD,modid = SplashMilk.MOD_ID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = SplashMilk.MOD_ID, value = Dist.CLIENT)
 public class RendererRegistry {
 
     @OnlyIn(Dist.CLIENT)
